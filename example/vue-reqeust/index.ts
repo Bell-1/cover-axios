6186c362cc5fd797ea94f9fd5f2b5dccdf0cb6be
Bell_中元 <beiyj@publink.cn>
2022-03-02 09:22:39 +0800

    修改example

--- example/vue-reqeust/index.ts
+++ example/vue-reqeust/index.ts
@@ -1,60 +1,33 @@
-import type { Store } from 'vuex'
-import HTTP, { Api } from 'cover-axios'
-import ApiList from '@/request/api'
-import { Message } from 'element-ui'
-import { checkPermission } from '@/utils/permission'
+import http, { setOptions } from 'cover-axios'
+import ApiList from './api'
 
-const coverCodes: Set<number> = new Set([]); // 用来覆盖错误弹框的错误码
+const coverCodes: Set<number> = new Set([]) // 用来覆盖错误弹框的错误码
+const __DEV__ = true
 
-// vue.use
-function install(Vue: any, opts: { store?: Store<any> }) {
-	const http = Vue.prototype.$http = new HTTP();
-
-	http.beforeRequest(beforeRequest);
-
-	if (process.env.NODE_ENV === 'development') {
-		// 本地代理指向配置 根据自己的环境配置
-		http.setBaseURL('/test');
-	}
-
-	if (ApiList && Array.isArray(ApiList)) {
-		// 添加接口列表
-		http.addApiList(ApiList);
-	}
-
-	if (opts.store && opts.store.commit) {
-		// 我这里传入了vuex对象，存到store里方便在action中调用
-		opts.store.commit('setHttp', http);
-	}
-
-	http.interceptors.request.use(requestIntercept, requestFail)
-	http.interceptors.response.use(responentIntercept, requestFail)
-
-	Vue.prototype.$coverReuestMsg = {
-		add: addCoverCode,
-		remove: removeCoverCode
-	}
+if (__DEV__) {
+	// 本地代理指向配置
+	setOptions({ baseURL: '/serve' })
 }
 
-function beforeRequest(api: Api): boolean {
-	if (api.meta && api.meta.permission) {
-		const pass = checkPermission(api.meta.permission);
-		return pass
-	}
-	return true
+if (ApiList && Array.isArray(ApiList)) {
+	// 添加接口列表
+	http.addApiList(ApiList)
 }
+
+http.interceptors.request.use(requestIntercept, requestFail)
+http.interceptors.response.use(responseIntercept, requestFail)
 
 // 添加覆盖弹框 错误码
-function addCoverCode(code: number) {
-	coverCodes.add(code);
+export function addCoverCode(code: number) {
+	coverCodes.add(code)
 }
 
 // 添加了覆盖的错误码 不需要了可以删除
-function removeCoverCode(code: number | undefined) {
+export function removeCoverCode(code: number | undefined) {
 	if (!code) {
-		coverCodes.clear();
+		coverCodes.clear()
 	} else {
-		coverCodes.delete(code);
+		coverCodes.delete(code)
 	}
 }
 
@@ -65,32 +38,27 @@ function requestIntercept(config: any) {
 
 // 返回拦截 这里根据自己的项目 做了返回处理
 // 错误会进行弹窗提示
-function responentIntercept(res) {
-	const { data, code, msg } = res.data;
+function responseIntercept(res: any) {
+	if (!res) return showMessage(res)
+	const { data, code } = res.data
 	if (code === 0) {
 		return data
 	} else {
-		showMessage(res.data);
-		return Promise.reject(msg);
+		return showMessage(res.data)
 	}
 }
 
 // 请求失败处理
-function requestFail(err) {
-	console.error('reqeust fail', err)
+function requestFail(err: any) {
+	console.error('request fail', err)
 	return Promise.reject(err)
 }
 
 // 错误弹窗 会通过coverCodes过滤是否使用弹框
-function showMessage(data: { msg: string, code: number }) {
-	if (coverCodes.has(data.code)) return;
-	Message({
-		message: data.msg || 'error',
-		type: 'error',
-		offset: 100,
-	});
-}
-
-export default {
-	install
+function showMessage(data: { msg: string; code: number }) {
+	if (data.code && !coverCodes.has(data.code)) {
+		// 显示全局错误提示弹框
+	}
+	console.error('http error message:', data.msg || data)
+	return Promise.reject(data.msg || data)
 }
